357e96e5e91a901aff0e51057ace01e8886365a5
Akash km <akashmanoj999@gmail.com>
2024-02-19 17:06:51 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,86 @@
+// let x=20;
+
+//console.log(x);
+//{
+  //  let x=50;
+    //console.log(x);
+//}
+//a=true
+//console.log(a);
+//console.log(typeof (a));
+// temp=20;
+// if(temp>30)
+// {
+//     console.log("it is a hot day");
+// }
+// else if(temp<=20 &&temp>20)
+// {
+//     console.log("it is cold day");
+// }
+// else{
+//     console.log("pleasent day");
+// }
+// let i=0;
+// while(i<6)
+// {
+//     console.log(i)
+//     i++;
+// }
+// let i=0;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<5)
+// let i=1;
+// while(i<=15)
+// {
+//     console.log(i);
+//     if(i%2==0)
+//     {
+//         console.log("even number");
+    
+    
+//     }
+//     else{
+//         console.log("odd number");
+
+//     }
+//         i++;
+//     }
+// let fruit=["Apple","Banana","Orange"];
+// console.log(fruit[1]);
+// console.log(fruit.length);
+// fruit[1]="grape";
+// console.log(fruit);
+// for(let i=0;i<fruit.length;i++)
+// {
+//     console.log(fruit[i]);
+// }
+// fruit.push("Kiwi");
+// console.log(fruit);
+// fruit.pop("kiwi")
+// console.log(fruit);
+// fruit.unshift("banana")
+// console.log(fruit);
+// fruit.shift("banana");
+// console.log(fruit);
+// function greet(name)
+// {
+//     console.log("welcome"+name);
+// }
+// greet("nanadana");
+// function add(a,b)
+// {
+//     return a+b;
+// }
+// console.log(add(2,3));
+// 
+// var add(a,b)=>{
+//     var sum=a+b;
+//     console.log(sum);
+//     console.log("sum is $(sum)")
+// }
+// add(40,50);
+
+// spread operator
